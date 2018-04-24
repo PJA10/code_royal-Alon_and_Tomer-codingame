68af1bfcf2dab4493fd78d2660fc03f6319a2c6e
taptomer <tomer.pat@gmail.com>
2018-04-25 00:16:42 +0300

    added towards function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import math
 import random
+import copy
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
@@ -11,6 +12,9 @@ class MapObj:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def __str__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
 
 class Site(MapObj):
     def __init__(self, Id, x, y, radius):
@@ -74,6 +78,8 @@ def main():
 
     # game loop
     for turn in range(1000):
+        test_loc = towards(MapObj(100, 100), MapObj(1000, 1000), 400)
+        print("test_loc", test_loc, file = sys.stderr)
         # touched_site: -1 if none
         gold, touched_site = [int(i) for i in input().split()]
         for i in range(num_sites):
@@ -103,28 +109,28 @@ def main():
         # Write an action using print
         # To debug: print("Debug messages...", file=sys.stderr)
         print("safe_point:", safe_point, file=sys.stderr)
-        if turn == 5:
-            safe_point = MapObj(my_queen.x, my_queen.y)
+        # if turn == 5:
+        #     safe_point = MapObj(my_queen.x, my_queen.y)
 
         my_barracks_list = get_my_barracks(sites_list)
         seeing_sold_out_barracks = set([site for site in sites_list if site.remaining_gold == 0])
         sold_out_mines.update(seeing_sold_out_barracks)
-        
+
         choose_action(sites_list, my_queen, creep_list, my_barracks_list, touched_site)
-        
+
         # if we have a barracks
         if len(my_barracks_list) > 0:
             print("TRAIN {0}".format(my_barracks_list[0].Id))
         else:
             print("TRAIN")
-            
+
 def choose_action(sites_list, my_queen, creep_list, my_barracks_list, touched_site):
     my_goldmines = [site for site in sites_list if site.structure_type == GOLDMINE and site.owner == FRIENDLY]
     income = sum(map(lambda x: x.income_rate, my_goldmines)) # cal our total income from all the mines
     print ('income:', income, file=sys.stderr)
     touched_site = get_touched_site(sites_list, touched_site) # get the actual site by its Id
-    enemy_creeps = [creep for creep in creep_list if creep.owner == ENEMY] 
-    
+    enemy_creeps = [creep for creep in creep_list if creep.owner == ENEMY]
+
     # if the queen toche a mine that isn't maxed
     if touched_site != None and touched_site.structure_type == GOLDMINE and touched_site.income_rate < touched_site.max_mine_rate:
         # upgrade the mine
@@ -134,7 +140,7 @@ def choose_action(sites_list, my_queen, creep_list, my_barracks_list, touched_si
         # then build a knight barracks on the closest posible site
         closest_site = get_closest_site_wothout_strucure(my_queen, sites_list)
         print ("BUILD {0} BARRACKS-KNIGHT".format(closest_site.Id) if closest_site != None else "MOVE {0} {1}".format(safe_point.x, safe_point.y))
-    # if we don't get enough gold per turn and we the queen is safe 
+    # if we don't get enough gold per turn and we the queen is safe
     elif income < WANTED_INCOME and is_safe(my_queen, enemy_creeps):
         # then build a mine on the closest posible site
         to_build_site = get_closest_possible_mine(my_queen, sites_list)
@@ -145,7 +151,30 @@ def choose_action(sites_list, my_queen, creep_list, my_barracks_list, touched_si
         if touched_site != None and touched_site.owner == FRIENDLY and touched_site.structure_type == TOWER and touched_site.hp < 700:
             to_build_site = touched_site
         print ("BUILD {0} TOWER".format(to_build_site.Id) if to_build_site != None else "MOVE {0} {1}".format(safe_point.x, safe_point.y))
-            
+
+
+def towards(object1, object2, dis):
+    obj1 = copy.deepcopy(object1)
+    obj2 = copy.deepcopy(object2)
+    if obj1.x == obj2.x:
+        x = obj1.x
+        if obj1.y >= obj2.y:
+            y =  obj1.y - dis
+        else:
+            y =  obj1.y + dis
+        return MapObj(x, y)
+    if obj2.x < obj1.x:
+        dis = distance(obj1, obj2) - dis
+        swap = obj1
+        obj1 = obj2
+        obj2 = swap
+    m = (obj1.y - obj2.y)/(obj1.x - obj2.x)
+    x_diff = math.sqrt((dis**2)/(1+m**2))
+    y_diff = m*x_diff
+    x = math.floor(obj1.x + x_diff)
+    y = math.floor(obj1.y + y_diff)
+    return MapObj(x, y)
+
 
 def distance(obj1, obj2):
     return math.sqrt((obj1.x - obj2.x)**2 + (obj1.y - obj2.y)**2)
@@ -170,12 +199,12 @@ def get_closest_possible_mine(my_queen, sites_list):
     if len(possible_mines_list) == 0:
         return None
     return possible_mines_list[0]
-    
+
 def eliminate_dangerous_sites(sites_list, possible_sites):
     for tower in [site for site in sites_list if site.structure_type == TOWER and site.owner == ENEMY]:
         possible_sites = [site for site in possible_sites if distance(site, tower) > tower.attack_range]
     return possible_sites
-    
+
 def get_touched_site(sites_list, touched_site):
     touched_site = [site for site in sites_list if site.Id == touched_site]
     if len(touched_site) != 0:
